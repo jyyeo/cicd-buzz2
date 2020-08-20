bac47fd71a1e12a82da34265cf9122c6547a3c6a
Yeo Jin Yan <jinyan@Yeos-MacBook-Pro.local>
2020-08-20 12:49:35 +0800

    added image

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def generate_buzz():
-    page = '<html><body><h1>'
-    page += generator.generate_buzz()
-    page += '</h1></body></html>'
-    return page
+    # page = '<html><body><h1>'
+    # page += generator.generate_buzz()
+    # page += '</h1></body></html>'
+    # return page
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
